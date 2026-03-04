63f82f50070d190d3f03512dd94009a2467fb890
burntpiano <rory.c.blair@gmail.com>
2026-03-03 22:38:17 -0600

    cleaned up some

--- assignment/src/components/index.js
+++ assignment/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as ClickCounter } from './ClickCounter.jsx';
 export { default as Greeting } from './Greeting.jsx';
-export { default as TaskForm } from './taskComps/TaskForm.jsx';
+export { default as TaskForm } from './task-components/TaskForm.jsx';
 export { default as UserInfo } from './UserInfo.jsx';
-export { default as TaskComponent } from './taskComps/TaskComponent.jsx';
+export { default as TaskComponent } from './task-components/TaskComponent.jsx';
---
+++ assignment/src/components/task-components/TaskComponent.jsx
@@ -0,0 +1,24 @@
+function TaskComponent ({taskArr}) {
+    function getRandomTask() {
+        const randomIndex = Math.floor(Math.random() * taskArr.length);
+        return taskArr[randomIndex];
+    }
+    
+    const [task] = useState(getRandomTask());
+
+    return (
+        <div>
+            <h3>
+                These are the tasks that need to be done:
+                <ul>
+                    {taskArr.map((task, index) => <li key={index}>{task} - {index + 1}</li>)}
+                </ul>
+            </h3>
+            <h3>
+                And you will start with: {task}
+            </h3>
+        </div>
+    )
+}
+
+export default TaskComponent;
