4806efda15d655f48258aa05bec7a5a025e2b132
burntpiano <rory.c.blair@gmail.com>
2026-03-03 23:34:52 -0600

    cleaned up some

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -5,29 +5,6 @@ function handleClick() {
         alert("Hah! Spooked ya!");
     }
 
-// function TaskComponent ({taskArr}) {
-//     function getRandomTask() {
-//         const randomIndex = Math.floor(Math.random() * taskArr.length);
-//         return taskArr[randomIndex];
-//     }
-    
-//     const [task] = useState(getRandomTask());
-
-//     return (
-//         <div>
-//             <h3>
-//                 These are the tasks that need to be done:
-//                 <ul>
-//                     {taskArr.map((task, index) => <li key={index}>{task} - {index + 1}</li>)}
-//                 </ul>
-//             </h3>
-//             <h3>
-//                 And you will start with: {task}
-//             </h3>
-//         </div>
-//     )
-// }
-
 const taskArr = [
 "Do the dishes",
 "Take out the trash",
